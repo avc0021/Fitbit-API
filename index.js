0a5b6cbb1ff5d02d9afbfa646075837bc25fe9da
avc0021 <91796423+avc0021@users.noreply.github.com>
2022-11-16 16:26:05 -0600

    a.t. updated

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-// 1. This is for sleep
+// // 1. This is for sleep
 // const access_token =
-//   "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMzhWWkIiLCJzdWIiOiI1Mjk1UTIiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJ3aHIgd3BybyB3bnV0IHdzbGUgd3NvYyB3YWN0IHdveHkgd3RlbSB3d2VpIHdzZXQgd2xvYyB3cmVzIiwiZXhwIjoxNjY4NjM1MzQwLCJpYXQiOjE2NjgwMzA1NDB9.pgD9lzZDXtUVR94oU31oDFTuffhyycck4-aSejj6WRU";
+//   "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMzhWWkIiLCJzdWIiOiI1Mjk1UTIiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJ3aHIgd251dCB3cHJvIHdzbGUgd3NvYyB3YWN0IHdveHkgd3RlbSB3d2VpIHdzZXQgd2xvYyB3cmVzIiwiZXhwIjoxNjY5MjQxNDM1LCJpYXQiOjE2Njg2MzcwODh9.3voBFHr_4t5gzA-1XQSp7mYNZgOwrtLOeHxPPMCjpHM";
 
-// fetch("https://api.fitbit.com/1/user/5295Q2/sleep/date/2022-10-01/2022-10-10.json", {
+// fetch("https://api.fitbit.com/1/user/5295Q2/sleep/date/2022-10-02.json", {
 //   method: "GET",
 //   headers: { Authorization: "Bearer " + access_token },
 // })
@@ -12,7 +12,7 @@
 
 // 3. Minutes of exercise (divided into preset heart rate zones) // 4. Active zones mins
 // const access_token =
-//   "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMzhWWkIiLCJzdWIiOiI1Mjk1UTIiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJ3aHIgd3BybyB3bnV0IHdzbGUgd3NvYyB3YWN0IHdveHkgd3RlbSB3d2VpIHdzZXQgd2xvYyB3cmVzIiwiZXhwIjoxNjY4NjM1MzQwLCJpYXQiOjE2NjgwMzA1NDB9.pgD9lzZDXtUVR94oU31oDFTuffhyycck4-aSejj6WRU";
+//   "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMzhWWkIiLCJzdWIiOiI1Mjk1UTIiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJ3aHIgd251dCB3cHJvIHdzbGUgd3NvYyB3YWN0IHdveHkgd3RlbSB3d2VpIHdzZXQgd2xvYyB3cmVzIiwiZXhwIjoxNjY5MjQxNDM1LCJpYXQiOjE2Njg2MzcwODh9.3voBFHr_4t5gzA-1XQSp7mYNZgOwrtLOeHxPPMCjpHM";
 
 // fetch("https://api.fitbit.com/1/user/5295Q2/activities/list.json?beforeDate=2022-10-27&sort=asc&offset=0&limit=100", {
 //   method: "GET",
@@ -22,15 +22,15 @@
 //   .then((json) => console.log(json));
 
 //4. Cardio Fitness score
-const access_token =
-  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMzhWWkIiLCJzdWIiOiI1Mjk1UTIiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJ3aHIgd3BybyB3bnV0IHdzbGUgd3NvYyB3YWN0IHdveHkgd3RlbSB3d2VpIHdzZXQgd2xvYyB3cmVzIiwiZXhwIjoxNjY4NjM1MzQwLCJpYXQiOjE2NjgwMzA1NDB9.pgD9lzZDXtUVR94oU31oDFTuffhyycck4-aSejj6WRU";
+// const access_token =
+//   "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMzhWWkIiLCJzdWIiOiI1Mjk1UTIiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJ3aHIgd251dCB3cHJvIHdzbGUgd3NvYyB3YWN0IHdveHkgd3RlbSB3d2VpIHdzZXQgd2xvYyB3cmVzIiwiZXhwIjoxNjY5MjQxNDM1LCJpYXQiOjE2Njg2MzcwODh9.3voBFHr_4t5gzA-1XQSp7mYNZgOwrtLOeHxPPMCjpHM";
 
-fetch("https://api.fitbit.com/1/user/5295Q2/cardio_fitness/date/2022-10-10.json", {
-  method: "GET",
-  headers: { Authorization: "Bearer " + access_token },
-})
-  .then((response) => response.json())
-  .then((json) => console.log(json));
+// fetch("https://api.fitbit.com/1/user/5295Q2/cardio_fitness/date/2022-10-10.json", {
+//   method: "GET",
+//   headers: { Authorization: "Bearer " + access_token },
+// })
+//   .then((response) => response.json())
+//   .then((json) => console.log(json));
 
 
 
@@ -52,7 +52,7 @@ fetch("https://api.fitbit.com/1/user/5295Q2/cardio_fitness/date/2022-10-10.json"
 // Main example
 //This token expires every 30 days. We will need to manually updated Oct-28-2022.
 // const access_token =
-//   "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMzhWWkIiLCJzdWIiOiI1Mjk1UTIiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJ3aHIgd251dCB3cHJvIHdzbGUgd3NvYyB3YWN0IHdveHkgd3RlbSB3d2VpIHdzZXQgd3JlcyB3bG9jIiwiZXhwIjoxNjY3MDU1NDgyLCJpYXQiOjE2NjQ0NjM0ODJ9.u4sgRaTARrh3vlr4GH5q0sLvPXcrKo5o8SD-6xgz158";
+//   "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMzhWWkIiLCJzdWIiOiI1Mjk1UTIiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJ3aHIgd251dCB3cHJvIHdzbGUgd3NvYyB3YWN0IHdveHkgd3RlbSB3d2VpIHdzZXQgd2xvYyB3cmVzIiwiZXhwIjoxNjY5MjQxNDM1LCJpYXQiOjE2Njg2MzcwODh9.3voBFHr_4t5gzA-1XQSp7mYNZgOwrtLOeHxPPMCjpHM";
 
 // fetch("https://api.fitbit.com/1/user/5295Q2/activities/steps/date/2022-10-23/7d.json", {
 //   method: "GET",
@@ -60,3 +60,13 @@ fetch("https://api.fitbit.com/1/user/5295Q2/cardio_fitness/date/2022-10-10.json"
 // })
 //   .then((response) => response.json())
 //   .then((json) => console.log(json));
+
+const access_token =
+  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMzhWWkIiLCJzdWIiOiI1Mjk1UTIiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJ3aHIgd251dCB3cHJvIHdzbGUgd3NvYyB3YWN0IHdveHkgd3RlbSB3d2VpIHdzZXQgd2xvYyB3cmVzIiwiZXhwIjoxNjY5MjQxNDM1LCJpYXQiOjE2Njg2MzcwODh9.3voBFHr_4t5gzA-1XQSp7mYNZgOwrtLOeHxPPMCjpHM";
+
+fetch("https://api.fitbit.com/1/user/5295Q2/devices.json", {
+  method: "GET",
+  headers: { Authorization: "Bearer " + access_token },
+})
+  .then((response) => response.json())
+  .then((json) => console.log(json));
