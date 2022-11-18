1c1bea9a7619a4563a5c8b597a15866aaae8dbe8
avc0021 <91796423+avc0021@users.noreply.github.com>
2022-11-18 12:58:16 -0600

    adjusted back end, cardio fitness score now working

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-// // 1. This is for sleep
-// const access_token =
-//   "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMzhWWkIiLCJzdWIiOiI1Mjk1UTIiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJ3aHIgd251dCB3cHJvIHdzbGUgd3NvYyB3YWN0IHdveHkgd3RlbSB3d2VpIHdzZXQgd2xvYyB3cmVzIiwiZXhwIjoxNjY5MjQxNDM1LCJpYXQiOjE2Njg2MzcwODh9.3voBFHr_4t5gzA-1XQSp7mYNZgOwrtLOeHxPPMCjpHM";
-
-// fetch("https://api.fitbit.com/1/user/5295Q2/sleep/date/2022-10-02.json", {
-//   method: "GET",
-//   headers: { Authorization: "Bearer " + access_token },
-// })
-//   .then((response) => response.json())
-//   .then((json) => console.log(json));
-
 
 // 3. Minutes of exercise (divided into preset heart rate zones) // 4. Active zones mins
 // const access_token =
@@ -22,15 +11,15 @@
 //   .then((json) => console.log(json));
 
 //4. Cardio Fitness score
-// const access_token =
-//   "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMzhWWkIiLCJzdWIiOiI1Mjk1UTIiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJ3aHIgd251dCB3cHJvIHdzbGUgd3NvYyB3YWN0IHdveHkgd3RlbSB3d2VpIHdzZXQgd2xvYyB3cmVzIiwiZXhwIjoxNjY5MjQxNDM1LCJpYXQiOjE2Njg2MzcwODh9.3voBFHr_4t5gzA-1XQSp7mYNZgOwrtLOeHxPPMCjpHM";
+const access_token =
+  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMzhWWkIiLCJzdWIiOiI1Mjk1UTIiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJ3aHIgd3BybyB3bnV0IHdzbGUgd3NvYyB3YWN0IHdveHkgd3RlbSB3d2VpIHdjZiB3c2V0IHdsb2Mgd3JlcyIsImV4cCI6MTY2OTM5NDYzOSwiaWF0IjoxNjY4Nzg5ODM5fQ.fmIlBGKBKz57xDFznP3y2wANHa83rCnpQwei0FC2HzI";
 
-// fetch("https://api.fitbit.com/1/user/5295Q2/cardio_fitness/date/2022-10-10.json", {
-//   method: "GET",
-//   headers: { Authorization: "Bearer " + access_token },
-// })
-//   .then((response) => response.json())
-//   .then((json) => console.log(json));
+fetch("https://api.fitbit.com/1/user/5295Q2/cardioscore/date/2022-10-10/2022-10-31.json", {
+  method: "GET",
+  headers: { Authorization: "Bearer " + access_token },
+})
+  .then((response) => response.json())
+  .then((json) => console.log(json));
 
 
 
@@ -61,12 +50,12 @@
 //   .then((response) => response.json())
 //   .then((json) => console.log(json));
 
-const access_token =
-  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMzhWWkIiLCJzdWIiOiI1Mjk1UTIiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJ3aHIgd251dCB3cHJvIHdzbGUgd3NvYyB3YWN0IHdveHkgd3RlbSB3d2VpIHdzZXQgd2xvYyB3cmVzIiwiZXhwIjoxNjY5MjQxNDM1LCJpYXQiOjE2Njg2MzcwODh9.3voBFHr_4t5gzA-1XQSp7mYNZgOwrtLOeHxPPMCjpHM";
+// const access_token =
+//   "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMzhWWkIiLCJzdWIiOiI1Mjk1UTIiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJ3aHIgd251dCB3cHJvIHdzbGUgd3NvYyB3YWN0IHdveHkgd3RlbSB3d2VpIHdzZXQgd2xvYyB3cmVzIiwiZXhwIjoxNjY5MjQxNDM1LCJpYXQiOjE2Njg2MzcwODh9.3voBFHr_4t5gzA-1XQSp7mYNZgOwrtLOeHxPPMCjpHM";
 
-fetch("https://api.fitbit.com/1/user/5295Q2/devices.json", {
-  method: "GET",
-  headers: { Authorization: "Bearer " + access_token },
-})
-  .then((response) => response.json())
-  .then((json) => console.log(json));
+// fetch("https://api.fitbit.com/1/user/5295Q2/devices.json", {
+//   method: "GET",
+//   headers: { Authorization: "Bearer " + access_token },
+// })
+//   .then((response) => response.json())
+//   .then((json) => console.log(json));
